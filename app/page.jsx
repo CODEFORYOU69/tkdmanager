711d7b1d521cf5224b3877e3b908c58b892e553e
OUASMI YOUNES <y.ouasmi@gmail.com>
2024-04-16 13:39:43 +0200

    vercel deploy

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,22 @@
+import { ThemeProvider } from '@emotion/react';
 import { SnackbarProvider } from 'notistack';
+import theme from './theme/theme';
 import { CssBaseline } from '@mui/material';
 import Layout from './layout';
-import React, { ReactNode } from 'react';
 import MobileNavbar from '../components/MobileNavbar';
 
-interface PageProps {
-  children: ReactNode;
-}
-
-const Page: React.FC<PageProps> = ({ children }) => {
+function Page({ children }) {
   return (
     <SnackbarProvider maxSnack={3}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <Layout>
-          <MobileNavbar />
-          {children}
+          <div>
+            <MobileNavbar />
+            {children}
+          </div>
         </Layout>
+      </ThemeProvider>
     </SnackbarProvider>
   );
 }
