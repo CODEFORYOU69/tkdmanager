e645e8c8d52f6944976b80986403eb3412b462f6
OUASMI YOUNES <y.ouasmi@gmail.com>
2024-04-16 20:49:46 +0200

    change add fight logic

--- components/AddFightModal.js
+++ components/AddFightModal.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Dialog, DialogActions, DialogContent, DialogTitle, TextField, Button, MenuItem, Select, InputLabel, FormControl } from '@mui/material';
+import { Dialog, DialogActions, DialogContent, DialogTitle, TextField, Button, MenuItem, Select, InputLabel, FormControl, IconButton } from '@mui/material';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 
 const AddFightModal = ({ open, onClose, competitionId }) => {
-    const [fightNumber, setFightNumber] = useState('');
-    const [color, setColor] = useState('');
+    const [fights, setFights] = useState([{ fightNumber: '', color: '' }]);
     const [fighters, setFighters] = useState([]);
-    const [selectedFighterId, setSelectedFighterId] = useState('');  // Initialisation à chaîne vide ou autre valeur par défaut
+    const [selectedFighterId, setSelectedFighterId] = useState('');
+    console.log("selectedFighterId", selectedFighterId);
+    console.log("fights", fights);
 
     useEffect(() => {
-        fetch('/api/fighters')  // Assurez-vous que cette URL est correcte et que l'API renvoie les données attendues
+        fetch('/api/fighters')
             .then(response => response.json())
             .then(setFighters)
             .catch(error => console.error('Error fetching fighters:', error));
@@ -19,48 +22,42 @@ const AddFightModal = ({ open, onClose, competitionId }) => {
             alert('Please select a fighter');
             return;
         }
+
         const response = await fetch('/api/match/addFight', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                fighterId: selectedFighterId,  // Utilisation de l'ID du combattant sélectionné
-                competitionId,  
-                fightNumber,
-                color,
+                fighterId: selectedFighterId,
+                competitionId,
+                fights,
             }),
         });
 
         if (response.ok) {
-            onClose(true);  // Ferme la modal et rafraîchit éventuellement les données parentes
+            onClose(true);
         } else {
-            alert('Failed to add fighter to competition');
+            alert('Failed to add fights to competition');
         }
+    };
+
+    const handleAddFight = () => {
+        setFights([...fights, { fightNumber: '', color: '' }]);
+    };
+
+    const handleRemoveFight = (index) => {
+        setFights(fights.filter((_, i) => i !== index));
+    };
+
+    const handleChangeFight = (index, field, value) => {
+        const newFights = [...fights];
+        newFights[index][field] = value;
+        setFights(newFights);
     };
 
     return (
         <Dialog open={open} onClose={() => onClose(false)}>
-            <DialogTitle>Add Fighter to Competition</DialogTitle>
+            <DialogTitle>Add Multiple Fights for a Fighter</DialogTitle>
             <DialogContent>
-                <TextField
-                    fullWidth
-                    margin="normal"
-                    label="Fight Number"
-                    type="number"
-                    value={fightNumber}
-                    onChange={(e) => setFightNumber(e.target.value)}
-                />
-                <FormControl fullWidth margin="normal">
-                    <InputLabel id="color-label">Color</InputLabel>
-                    <Select
-                        value={color}
-                        label="Color"
-                        onChange={(e) => setColor(e.target.value)}
-                        labelId="color-label"
-                    >
-                        <MenuItem value="Blue">Blue</MenuItem>
-                        <MenuItem value="Red">Red</MenuItem>
-                    </Select>
-                </FormControl>
                 <FormControl fullWidth margin="normal">
                     <InputLabel id="fighter-label">Fighter</InputLabel>
                     <Select
@@ -68,7 +65,6 @@ const AddFightModal = ({ open, onClose, competitionId }) => {
                         value={selectedFighterId}
                         label="Fighter"
                         onChange={(e) => setSelectedFighterId(e.target.value)}
-                        fullWidth
                     >
                         {fighters.map((fighter) => (
                             <MenuItem key={fighter.id} value={fighter.id}>
@@ -77,10 +73,37 @@ const AddFightModal = ({ open, onClose, competitionId }) => {
                         ))}
                     </Select>
                 </FormControl>
+                {fights.map((fight, index) => (
+                    <div key={index} style={{ display: 'flex', alignItems: 'center', marginTop: 10 }}>
+                        <TextField
+                            label="Fight Number"
+                            type="number"
+                            value={fight.fightNumber}
+                            onChange={(e) => handleChangeFight(index, 'fightNumber', e.target.value)}
+                            style={{ marginRight: 10 }}
+                        />
+                        <Select
+                            value={fight.color}
+                            onChange={(e) => handleChangeFight(index, 'color', e.target.value)}
+                            style={{ marginRight: 10 }}
+                        >
+                            <MenuItem value="Blue">Blue</MenuItem>
+                            <MenuItem value="Red">Red</MenuItem>
+                        </Select>
+                        {index > 0 && (
+                            <IconButton onClick={() => handleRemoveFight(index)}>
+                                <RemoveCircleIcon />
+                            </IconButton>
+                        )}
+                    </div>
+                ))}
+                <Button startIcon={<AddCircleIcon />} onClick={handleAddFight}>
+                    Add Another Fight
+                </Button>
             </DialogContent>
             <DialogActions>
                 <Button onClick={() => onClose(false)}>Cancel</Button>
-                <Button onClick={handleSave}>Add</Button>
+                <Button onClick={handleSave}>Save All Fights</Button>
             </DialogActions>
         </Dialog>
     );
--- pages/api/match/addFight.js
+++ pages/api/match/addFight.js
@@ -3,23 +3,31 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-        const { competitionId, fighterId, fightNumber, color } = req.body;
-        console.log("req.body:", req.body);
+        const { fighterId, competitionId, fights } = req.body;  // Extraction des données à partir de la structure que vous avez fournie
+
+        // Filtre pour éliminer les combats non remplis correctement
+        const validFights = fights.filter(fight => fight.fightNumber && fight.color);
 
         try {
-            // Create a new match or round record in the database
-            const newMatch = await prisma.match.create({
-                data: {
+            if (validFights.length === 0) {
+                return res.status(400).json({ message: "No valid fights provided." });
+            }
+
+            // Création de multiples matchs en utilisant `createMany`
+            const newMatches = await prisma.match.createMany({
+                data: validFights.map(fight => ({
                     competitionId,
                     fighterId,
-                    fightNumber: parseInt(fightNumber, 10),
-                    color,
-                }
+                    fightNumber: parseInt(fight.fightNumber, 10),
+                    color: fight.color,
+                })),
+                skipDuplicates: true, // Ignorer les doublons si nécessaire
             });
-            res.status(200).json(newMatch);
+
+            res.status(201).json(newMatches);
         } catch (error) {
-            console.error('Failed to add fight to competition:', error);
-            res.status(500).json({ message: 'Failed to add fight to competition', error: error.message });
+            console.error('Failed to add fights to competition:', error);
+            res.status(500).json({ message: 'Failed to add fights to competition', error: error.message });
         }
     } else {
         res.setHeader('Allow', ['POST']);
